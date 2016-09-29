9ed950b30ffe4d622534f2b35e67c1aff994d654
Marco Felici <marcofelici91@gmail.com>
2016-09-29 15:38:24 +0200

    Add standalone progra to import feeds into DB

---
+++ importfeeds/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+var (
+	domain     string = "tcp"
+	address    string = ":6379"
+	filepath   string
+	socketpath string
+)
+
+func init() {
+	flag.StringVar(&filepath, "i", "", "Specifies the path to the file to be imported.")
+	flag.StringVar(&socketpath, "s", "", "Connects to redis using the socket provided.")
+}
+
+func main() {
+	flag.Parse()
+
+	if filepath == "" {
+		log.Fatal("You need to specify a file to import: bootstrap -i /path/to/file")
+	}
+
+	if socketpath != "" {
+		domain = "unix"
+		address = socketpath
+	}
+
+	c, err := redis.Dial(domain, address)
+	if err != nil {
+		log.Fatal("redis:", err)
+	}
+	defer c.Close()
+
+	err = ImportFeeds(c, filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
